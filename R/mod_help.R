a61aeb7895a8fba9c4a4684cf42f019ca3f3c4bd
Rico Derks <r.j.e.derks@lumc.nl>
2024-07-19 10:00:05 +0200

    Start with help documentation.

--- R/mod_help.R
+++ R/mod_help.R
@@ -7,10 +7,40 @@
 #' @noRd
 #'
 #' @importFrom shiny NS tagList
+#' @importFrom bslib navset_card_tab nav_panel
 mod_help_ui <- function(id){
   ns <- NS(id)
   tagList(
-    shiny::p("Help information will be put here!")
+    bslib::navset_card_tab(
+      bslib::nav_panel(
+        title = "Data",
+        shiny::p("In the ", shiny::strong("Data"), "sheet you can upload your meta data and raw data."),
+        shiny::h3("Meta data"),
+        shiny::p("In the meta data part you have to define several columns."),
+        shiny::HTML("<ul>
+                      <li><b>Sample ID:</b> column with sample ID's</li>
+                      <li><b>Sample type:</b> column with sample type, i.e. blank, pooled sample, sample, etc.</li>
+                      <li><b>Acquisition order:</b> column with the acquisition order. This is over all batches.</li>
+                      <li><b>Batch:</b> column with batch information.</li>
+                    </ul>"),
+        shiny::p("The app tries to recognize several columns, but please check them."),
+        shiny::p("The ", shiny::strong("text patterns"), "need to be defined by to recognize from the ", shiny::strong("sample type column"), "which samples is which.",
+                 "Regular expression are use for the recognition. After loading the meta data the two graphs at the bottom will show how the different sample types are recognized.",
+                 "The left graph shows the different sample types and the count. The right graph will show the different sample types and the count, after applying the filtering,
+                 based on the text patterns. It is important to have:"),
+        shiny::HTML("<ul>
+                      <li>Blanks</li>
+                      <li>Pooled samples</li>
+                      <li>Samples</li>
+                    </ul>")
+      ),
+      bslib::nav_panel(
+        title = "Data overview"
+      ),
+      bslib::nav_panel(
+        title = "Batch correction"
+      )
+    )
   )
 }
 
